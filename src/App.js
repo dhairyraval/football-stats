9a6c02b426357a5f2809db26cae6ff7355b7c687
Nuey2310 <dhairyraval@gmail.com>
2021-06-27 03:30:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
 // import "./App.css";
 
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 
 import HomePage from "./components/HomePage";
 import Standings from "./components/Standings";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/standings" component={Standings} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
